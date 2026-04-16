87ff897ddb3820a73e5b85311af0e4d287d49c32
BogdanATA <batameev@gmail.com>
2026-04-16 12:12:39 -0400

    - created DoWhileLoop class and used do while loop to print I love potatoes 5 times

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -1,15 +1,13 @@
 package com.pluralsight;
 
-public class WhileLoop {
+public class DoWhileLoop {
     public static void main(String[] args) {
 
         //print i love potatoes 5 times
         int i = 0;
-        while (i < 5){
+        do{
             System.out.println("I love potatoes!");
             i++;
-        }
-
-
+        } while (i < 5);
     }
 }
