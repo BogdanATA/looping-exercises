136901baa68d896c8baac4a1383bb56536e07347
BogdanATA <batameev@gmail.com>
2026-04-16 12:28:28 -0400

    - added the 1 second timer

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,12 +1,13 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
         //countdown 10 to 1 with for loop
         for (int i = 10; i >= 0; i--){
             if (i > 0){
                 System.out.println(i);
+                Thread.sleep(1000);
             } else{
                 System.out.println("Launch");
             }
